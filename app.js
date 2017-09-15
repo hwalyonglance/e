13039a0fa381118c97daa484c4b15100cd109db0
RusmanWahab <hwalyonglance@gmail.com>
2017-09-15 07:46:38 +0700

    fc

---
+++ app.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+var express = require('express');
+var io = require('socket.io');
+var socket
+var siofu = require("socketio-file-upload");
+var app = express();
+    // .use(siofu.router)
+
+app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+
+})
+
+var server = http.createServer(app)
+var $Socket = io(server);
+
+$Socket.of('/cate').on("connection", function(_socket){
+		var uploader = new siofu();
+		uploader.dir = path.join(__dirname, 'public', 'uploads');
+		uploader.listen(_socket);
+		uploader.on("saved", function(event){
+			console.log(event)
+			const ext = event.file.name.split('.');
+			console.log(event.file.base + '.' + ext[ ext.length - 1 ])
+		});
+		console.log('/cate')
+});
+$Socket.of('/item').on('connection', (_socket) => {
+	console.log('/item')
+})
+
+server.listen(3000)
+server.on('listening', () => {
+	console.log('http://localhost:3000');
+});
+
+fs.unlink(path.join(__dirname, 'public', 'uploads', 'swio.png'), (err) => {
+	console.log(err)
+});
